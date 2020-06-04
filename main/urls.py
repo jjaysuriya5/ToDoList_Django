503156ad573f005e3dc58cb9a7a049304741725d
jjaysuriya5 <53468162+jjaysuriya5@users.noreply.github.com>
2020-06-04 13:53:16 +0530

    Add files via upload

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+ 
+    path( '', views.home , name = 'home' ),
+    path( '<int:id>', views.index , name = 'index' ),
+    path( 'create/', views.create , name = 'home' ),
+    path("view/", views.view, name="view"),
+    
+]
